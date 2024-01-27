fb448da10dff5910b0bdf164b2c72703caeeb5d5
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:47:18 +0500

    User Model

---
+++ Backend/src/Registration.Domain/Entities/Users/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Registration.Domain.Entities.Users
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+    }
+}
