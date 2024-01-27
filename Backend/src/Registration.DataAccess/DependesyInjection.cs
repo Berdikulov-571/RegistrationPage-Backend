b82e7233bede5fe0ddf23d556ff3e4e49f03f998
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:46:09 +0500

    DependecyInjection

---
+++ Backend/src/Registration.DataAccess/DependesyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Registration.DataAccess.Persistance.DataContexts;
+using Registration.Service.Abstractions.DbContexts;
+
+namespace Registration.DataAccess
+{
+    public static class DependesyInjection
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
