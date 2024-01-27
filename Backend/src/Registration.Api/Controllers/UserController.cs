acb2597c847924eff87edd832c33523e20eb53ca
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:29:18 +0500

    User Controller

---
+++ Backend/src/Registration.Api/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Registration.Domain.Entities.Users;
+using Registration.Service.UseCases.Users.Commands;
+using Registration.Service.UseCases.Users.Queries;
+
+namespace Registration.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync(CreateUserCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            IEnumerable<User> users = await _mediator.Send(new GetAllUsersQuery());
+
+            return Ok(users);
+        }
+    }
+}
