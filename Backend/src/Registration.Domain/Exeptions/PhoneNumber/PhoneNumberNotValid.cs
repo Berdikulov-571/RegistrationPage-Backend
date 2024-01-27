8dc8a5a5370b113dcee44d710f1284f62fa4b88d
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:47:57 +0500

    Phone Number Not Valid

---
+++ Backend/src/Registration.Domain/Exeptions/PhoneNumber/PhoneNumberNotValid.cs
@@ -0,0 +1,10 @@
+﻿namespace Registration.Domain.Exeptions.PhoneNumber
+{
+    public class PhoneNumberNotValid : GlobalException
+    {
+        public PhoneNumberNotValid()
+        {
+            TitleMessage = "Phone Number Not Valid !";
+        }
+    }
+}
