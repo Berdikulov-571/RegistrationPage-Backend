4147633a51cbf545b1cae12286ac26281ce060fd
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:47:26 +0500

    Create User Command

---
+++ Backend/src/Registration.Service/UseCases/Users/Commands/CreateUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Registration.Domain.Dtos.Users;
+
+namespace Registration.Service.UseCases.Users.Commands
+{
+    public class CreateUserCommand : UserRegistration, IRequest<int>
+    {
+
+    }
+}
