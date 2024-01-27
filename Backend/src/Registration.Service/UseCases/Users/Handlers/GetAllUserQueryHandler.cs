72c13c2b0c45f52b53bb1297d0b4d34f2454e605
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:48:48 +0500

    Get All Query Handler

---
+++ Backend/src/Registration.Service/UseCases/Users/Handlers/GetAllUserQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Registration.Domain.Entities.Users;
+using Registration.Service.Abstractions.DbContexts;
+using Registration.Service.UseCases.Users.Queries;
+
+namespace Registration.Service.UseCases.Users.Handlers
+{
+    public class GetAllUserQueryHandler : IRequestHandler<GetAllUsersQuery, IEnumerable<User>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllUserQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<User>? users = await _context.Users.ToListAsync(cancellationToken);
+
+            return users;
+        }
+    }
+}
