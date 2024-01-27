8af49e744806a48c7efc9034109ab6dcfb814f39
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:46:18 +0500

    ApplicationDbContext

---
+++ Backend/src/Registration.DataAccess/Persistance/DataContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registration.Domain.Entities.Users;
+using Registration.Service.Abstractions.DbContexts;
+
+namespace Registration.DataAccess.Persistance.DataContexts
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
