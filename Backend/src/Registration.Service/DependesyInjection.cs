ff45ab9d66b8d27dbc6bd6bce4006003d20d52c7
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:49:08 +0500

    Dependecy Injection

---
+++ Backend/src/Registration.Service/DependesyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Registration.Service
+{
+    public static class DependesyInjection 
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
