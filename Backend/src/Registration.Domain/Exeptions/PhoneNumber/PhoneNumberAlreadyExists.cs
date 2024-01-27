e40d310d7b5cdc835bef515d01648d26189aa1bb
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:47:12 +0500

    PhoneNumberAlreadyExists

---
+++ Backend/src/Registration.Domain/Exeptions/PhoneNumber/PhoneNumberAlreadyExists.cs
@@ -0,0 +1,10 @@
+﻿namespace Registration.Domain.Exeptions.PhoneNumber
+{
+    public class PhoneNumberAlreadyExists : GlobalException
+    {
+        public PhoneNumberAlreadyExists()
+        {
+            TitleMessage = "Phone Number Already Exists!";
+        }
+    }
+}
