4ddc60befe3783e83e0284d597405d9d19c188a1
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:47:33 +0500

    IApplicationDbContext

---
+++ Backend/src/Registration.Service/Abstractions/DbContexts/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registration.Domain.Entities.Users;
+
+namespace Registration.Service.Abstractions.DbContexts
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
