b3c969e6155fbb733463033ba368f0aede82b3bd
berdikulov-571 <mygithub571@gmail.com>
2024-01-27 17:46:35 +0500

    UserRegistration

---
+++ Backend/src/Registration.Domain/Dtos/Users/UserRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Registration.Domain.Attibutes.Email;
+using Registration.Domain.Attibutes.Password;
+
+namespace Registration.Domain.Dtos.Users
+{
+    public class UserRegistration
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        [Email]
+        public string Email { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        [Password]
+        public string Password { get; set; } = default!;
+    }
+}
